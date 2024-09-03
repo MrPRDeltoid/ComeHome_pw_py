e9dc8f06187482430d8778f7ea689f495330a251
Peter Manton <pcmanton@gmail.com>
2024-09-03 10:02:32 -0700

    Update for better except line

--- tests_ui/search_page/map_section/map_layers_test.py
+++ tests_ui/search_page/map_section/map_layers_test.py
@@ -6,5 +6,4 @@ def test_layers_bar(search_page: SearchPage, setup_search_page):
     """Verify the layers bar contents"""
     exp_layers = ['Crime', 'Schools', 'Price', 'Forecast']
     expect(search_page.layerButtons).to_have_count(4)
-    for button in range(search_page.layerButtons.count()):
-        expect(search_page.layerButtons.nth(button)).to_have_text(exp_layers[button])
+    expect(search_page.layerButtons).to_have_text(exp_layers)
